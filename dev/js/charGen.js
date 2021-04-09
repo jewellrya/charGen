6308b5dfad2d02826ae3f0add55457f9ed43486a
Ryan Jewell <ryanjewell@eflexsystems.com>
2021-04-08 22:04:12 -0400

    derp

--- dev/js/charGen.js
+++ dev/js/charGen.js
@@ -7,39 +7,47 @@ ctx.imageSmoothingEnabled = false;
 var xOffset = 7;
 var yOffset = 5;
 
-var images_humanMale = [
-	{name: 'humanMale1', src: '../../assets/Human\ Male1.png', x: xOffset, y: yOffset}, 
-	{name: 'humanMale2', src: '../../assets/Human\ Male2.png', x: xOffset, y: yOffset},
-	{name: 'humanMale3', src: '../../assets/Human\ Male3.png', x: xOffset, y: yOffset}, 
-];
-var images_humanMaleHair = [
-	{name: 'humanMaleHair1', src: '../../assets/Human\ Male\ Hair1.png', x: xOffset+3, y: yOffset},
-	{name: 'humanMaleHair2', src: '../../assets/Human\ Male\ Hair2.png', x: xOffset+3, y: yOffset}, 
-];
-var images_humanMaleBeard = [
-	{name: 'humanMaleBeard1', src: '../../assets/Human\ Male\ Beard1.png', x: xOffset+3, y: yOffset+4}, 
-	{name: 'humanMaleBeard2', src: '../../assets/Human\ Male\ Beard2.png', x: xOffset+5, y: yOffset+7}, 
-];
-var images_humanMaleAdornment = [
-	{name: 'humanMaleAdornment1', src: '../../assets/Human\ Male\ Adornment1.png', x: xOffset+1, y: yOffset+1}, 
-];
-var images_humanMaleTattoo = [
-	{name: 'humanMaleTattoo1', src: '../../assets/Human\ Male\ Tattoo1.png', x: xOffset, y: yOffset+14},
-];
+var humanMale = [
+	[
+		{name: 'humanMale1', src: '../../assets/Human\ Male1.png', x: xOffset, y: yOffset}, 
+		{name: 'humanMale2', src: '../../assets/Human\ Male2.png', x: xOffset, y: yOffset},
+		{name: 'humanMale3', src: '../../assets/Human\ Male3.png', x: xOffset, y: yOffset},
+	],
+	[
+		{name: 'humanMaleHair_blank', src: '../../assets/_blank.png', x: 0, y: 0},
+		{name: 'humanMaleHair1', src: '../../assets/Human\ Male\ Hair1.png', x: xOffset+3, y: yOffset},
+		{name: 'humanMaleHair2', src: '../../assets/Human\ Male\ Hair2.png', x: xOffset+3, y: yOffset},
+	],
+	[
+		{name: 'humanMaleBeard_blank', src: '../../assets/_blank.png', x: 0, y: 0},
+		{name: 'humanMaleBeard1', src: '../../assets/Human\ Male\ Beard1.png', x: xOffset+3, y: yOffset+4}, 
+		{name: 'humanMaleBeard2', src: '../../assets/Human\ Male\ Beard2.png', x: xOffset+5, y: yOffset+7}, 
+	],
+	[
+		{name: 'humanMaleAdornment_blank', src: '../../assets/_blank.png', x: 0, y: 0},
+		{name: 'humanMaleAdornment1', src: '../../assets/Human\ Male\ Adornment1.png', x: xOffset+1, y: yOffset+1},
+	],
+	[
+		{name: 'humanMaleTattoo_blank', src: '../../assets/_blank.png', x: 0, y: 0},
+		{name: 'humanMaleTattoo1', src: '../../assets/Human\ Male\ Tattoo1.png', x: xOffset, y: yOffset+14},
+	],
+	[
+		{name: 'humanMaleArmor_blank', src: '../../assets/_blank.png', x: 0, y: 0},
+		{name: 'humanMaleHeavy1', src: '../../assets/Human\ Male\ Heavy1.png', x: xOffset-1, y: yOffset},
+		{name: 'humanMaleHeavy2', src: '../../assets/Human\ Male\ Heavy2.png', x: xOffset-4, y: yOffset},
+	],
+]
 
-var images_humanMaleHeavy = [
-	{name: 'humanMaleHeavy1', src: '../../assets/Human\ Male\ Heavy1.png', x: xOffset-1, y: yOffset},
-	{name: 'humanMaleHeavy2', src: '../../assets/Human\ Male\ Heavy2.png', x: xOffset-4, y: yOffset},
-];
+preload = function(imageArray, callback) {
 
-preload = function(imageArray, callback){
 	var imagesLoaded = 0;
 	var loadedImages = [];
 
 	for (var i = 0; i < imageArray.length; i++) {
-		var imgObj = new Image();
 
+		var imgObj = new Image();
 		imgObj.src = imageArray[i].src;
+		
 		loadedImages.push(imgObj);
 
 		imgObj.onload = function() {
@@ -51,7 +59,7 @@ preload = function(imageArray, callback){
 	}
 }
 
-function drawChar(imageArray) {
+function drawChar(imageArray, name) {
 	preload(imageArray, function(loadedImages){
 
 		for (var i = 0; i < imageArray.length; i++) {
@@ -59,25 +67,30 @@ function drawChar(imageArray) {
 		}
 
 		var img = canvas.toDataURL("image/png");
-		document.write('<img src="'+img+'"/><a href="'+img+'" download="derp">export png</a>');
+		var exportModule = '<div id="' + name + '" class="col-12 col-md-6 col-lg-3"><div class="d-flex flex-column"><img src="' + img + '"/><a class="text-center text-truncate" href="' + img + '" download="' + name + '">Export \"' + name + '\"</a></div></div>';
+		document.getElementById('charGen').innerHTML += exportModule;
 
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		
 	});
 }
 
-var genChar;
-for(var i = 0; i < images_humanMale.length; i++) {
-     for(var j = 0; j < images_humanMaleHair.length; j++) {
-		for(var k = 0; k < images_humanMaleBeard.length; k++) {
-			for(var l = 0; l < images_humanMaleAdornment.length; l++) {
-				for(var m = 0; m < images_humanMaleTattoo.length; m++) {
-					for(var n = 0; n < images_humanMaleHeavy.length; n++) {
 
-						genChar = [images_humanMale[i], images_humanMaleHair[j], images_humanMaleBeard[k], images_humanMaleAdornment[l], images_humanMaleTattoo[m], images_humanMaleHeavy[n]];
-						drawChar(genChar);
-						console.log(genChar);
-					}
+for(var i = 0; i < humanMale[0].length; i++) {
+     for(var j = 0; j < humanMale[1].length; j++) {
+		for(var k = 0; k < humanMale[2].length; k++) {
+			for(var l = 0; l < humanMale[3].length; l++) {
+				for(var m = 0; m < humanMale[4].length; m++) {
+						
+					var genChar = [];
+
+					genChar.push(humanMale[0][i]);
+					genChar.push(humanMale[1][j]);
+					genChar.push(humanMale[2][k]);
+					genChar.push(humanMale[3][l]);
+					genChar.push(humanMale[4][m]);
+
+					drawChar(genChar, 'humanMale' + (i + 1) + j + k + l + m);
 				}
 			}
 		}
